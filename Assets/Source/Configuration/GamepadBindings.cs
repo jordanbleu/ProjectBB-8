5787727bba56c857bc3c644aa82938522a402d4c
Sam <thesomeot@gmail.com>
2020-03-01 09:34:34 -0600

    update bindings

--- Assets/Source/Configuration/GamepadBindings.cs
+++ Assets/Source/Configuration/GamepadBindings.cs
@@ -13,18 +13,18 @@ namespace Assets.Source.Configuration
         public Dictionary<string, KeyCodeValue> Bindings { get; set; } = new Dictionary<string, KeyCodeValue>()
         {
             // Menu Controls
-            { InputConstants.K_MENU_UP,     new KeyCodeValue(GamepadConstants.GP_DPAD_V, KeyCodeValue.AxisDirections.Positive) },
-            { InputConstants.K_MENU_DOWN,   new KeyCodeValue(GamepadConstants.GP_DPAD_V, KeyCodeValue.AxisDirections.Negative) },
-            { InputConstants.K_MENU_LEFT,   new KeyCodeValue(GamepadConstants.GP_DPAD_H, KeyCodeValue.AxisDirections.Negative) },
-            { InputConstants.K_MENU_RIGHT,  new KeyCodeValue(GamepadConstants.GP_DPAD_H, KeyCodeValue.AxisDirections.Positive) },
-            { InputConstants.K_MENU_ENTER,  new KeyCodeValue(GamepadConstants.GP_ABUTTON) },
-            { InputConstants.K_MENU_BACK,   new KeyCodeValue(GamepadConstants.GP_BBUTTON) },
+            { InputConstants.K_MENU_UP,         new KeyCodeValue(GamepadConstants.GP_DPAD_V, KeyCodeValue.AxisDirections.Positive) },
+            { InputConstants.K_MENU_DOWN,       new KeyCodeValue(GamepadConstants.GP_DPAD_V, KeyCodeValue.AxisDirections.Negative) },
+            { InputConstants.K_MENU_LEFT,       new KeyCodeValue(GamepadConstants.GP_DPAD_H, KeyCodeValue.AxisDirections.Negative) },
+            { InputConstants.K_MENU_RIGHT,      new KeyCodeValue(GamepadConstants.GP_DPAD_H, KeyCodeValue.AxisDirections.Positive) },
+            { InputConstants.K_MENU_ENTER,      new KeyCodeValue(GamepadConstants.GP_ABUTTON) },
+            { InputConstants.K_MENU_BACK,       new KeyCodeValue(GamepadConstants.GP_BBUTTON) },
 
             // Player Controls
             { InputConstants.K_MOVE_LEFT,       new KeyCodeValue(GamepadConstants.GP_LEFTSTICK_H, KeyCodeValue.AxisDirections.Negative) },
             { InputConstants.K_MOVE_RIGHT,      new KeyCodeValue(GamepadConstants.GP_LEFTSTICK_H, KeyCodeValue.AxisDirections.Positive) },
-            { InputConstants.K_MOVE_DOWN,       new KeyCodeValue(GamepadConstants.GP_LEFTSTICK_V, KeyCodeValue.AxisDirections.Negative) },
-            { InputConstants.K_MOVE_UP,         new KeyCodeValue(GamepadConstants.GP_LEFTSTICK_V, KeyCodeValue.AxisDirections.Positive) },
+            { InputConstants.K_MOVE_DOWN,       new KeyCodeValue(GamepadConstants.GP_LEFTSTICK_V, KeyCodeValue.AxisDirections.Positive) },
+            { InputConstants.K_MOVE_UP,         new KeyCodeValue(GamepadConstants.GP_LEFTSTICK_V, KeyCodeValue.AxisDirections.Negative) },
             { InputConstants.K_ATTACK_PRIMARY,  new KeyCodeValue(GamepadConstants.GP_ABUTTON) },
             { InputConstants.K_PAUSE,           new KeyCodeValue(GamepadConstants.GP_START_BUTTON) },
             { InputConstants.K_DASH_LEFT,       new KeyCodeValue(GamepadConstants.GP_LEFTBUMPER) },

b8e7e3aa20740a2c038429035b6fbdbe2f018041
Jordan Bleu <jordanbleu1@gmail.com>
2020-03-20 06:21:10 -0500

    Stack sprites for the player creating 3d effect, add animator controller

---
+++ Assets/Source/Components/Player/Base/PlayerPartComponentBase.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Source.Components.Base;
+using Assets.Source.Components.Reactor.Interfaces;
+using UnityEngine;
+
+namespace Assets.Source.Components.Player.Base
+{
+    public abstract class PlayerPartComponentBase : ComponentBase, IProjectileReactor
+    {
+        public abstract int BaseDamage { get; }
+
+
+
+        public void ReactToProjectileHit(Collision2D collision, int baseDamage)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Source/Components/Player/PlayerPartComponent.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Source.Components.Base;
+using Assets.Source.Components.Reactor.Interfaces;
+using UnityEngine;
+
+namespace Assets.Source.Components.Player
+{
+    public class PlayerPartComponent : ComponentBase, IProjectileReactor
+    {
+        [SerializeField]
+        private float partDamageMultiplier = 1;
+
+        private PlayerBehavior playerBehavior;
+
+        public override void ComponentAwake()
+        {
+            playerBehavior = GetRequiredComponentInParent<PlayerBehavior>();
+            base.ComponentAwake();
+        }
+
+        public void ReactToProjectileHit(Collision2D collision, int baseDamage)
+        {
+            playerBehavior.ReactToHit(collision, baseDamage, partDamageMultiplier);
+        }
+    }
+}
